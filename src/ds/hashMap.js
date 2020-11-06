cdfa38f0923412aa0d4652df76d960f78ac76a8d
Adam Cardenas <cevaris@gmail.com>
2020-11-05 21:03:53 -0700

    put hashing

--- src/ds/hashMap.js
+++ src/ds/hashMap.js
@@ -1,8 +1,75 @@
 
 class HashMap {
+    constructor() {
+        this.buckets = new Array(10);
+        for (let i = 0; i < this.buckets.length; i++) {
+            this.buckets[i] = new Array();
+        }
+    }
+
+    /**
+     * Return number of KeyValues in HashMap.
+     */
     get length() {
         return 0;
     }
+
+    /**
+     * Inserts key and value into HashMap.
+     * @param {*} key 
+     * @param {*} value 
+     */
+    put(key, value) {
+        const hashNumber = hashCode(key);
+        const bucketIdx = hashNumber % this.buckets.length;
+        console.log(key, hashNumber, bucketIdx);
+    }
+
+    /**
+     * Retrieves a value from a HashMap, given the key.
+     * @param {*} key 
+     */
+    get(key) {
+    }
+
+    /**
+     * Remove value from HashMap that is associated.
+     * @param {*} key 
+     */
+    remove(key) {
+    }
+
+
 }
 
 module.exports = HashMap;
+
+
+/**
+ * Generates a int53 number hashCode for a given object.
+ * @param {number} key 
+ */
+function hashCode(key) {
+    const str = JSON.stringify(key);
+
+    // https://stackoverflow.com/a/52171480/3538289
+    const seed = 0;
+    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+    for (let i = 0, ch; i < str.length; i++) {
+        ch = str.charCodeAt(i);
+        h1 = Math.imul(h1 ^ ch, 2654435761);
+        h2 = Math.imul(h2 ^ ch, 1597334677);
+    }
+    h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^ Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+    h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^ Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+    return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+}
+
+
+// const hash = hashFunc(key) -> number
+// const bucket = hash % bucketSize;
+
+// const hash = hashFunc("someKey") // 123123123112
+// const bucket = 123123123112 % 10;
+// [][]["someKey"][][][][][][][]
+
--- tests/ds/hashMap.test.js
+++ tests/ds/hashMap.test.js
@@ -4,3 +4,15 @@ test('create empty HashMap', () => {
     const map = new HashMap();
     expect(map.length).toBe(0);
 });
+
+test('add key values to HashMap', () => {
+    const map = new HashMap();
+    
+    map.put('test', 1);
+    map.put(333, 2);
+    map.put({ obj: true }, 3);
+    map.put({ obj: true }, 3);
+    map.put({ obj: true }, 3);
+
+    expect(map.length).toBe(0);
+});
